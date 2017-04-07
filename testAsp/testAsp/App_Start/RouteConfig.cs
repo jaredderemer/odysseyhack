5dc48ff65a789efc7741bd155d483fbb5769ff55
Paul Antonio <paul.antonio2017@gmail.com>
2017-04-06 21:54:51 -0500

    Updated NuGet Packages

--- testAsp/testAsp/App_Start/RouteConfig.cs
+++ testAsp/testAsp/App_Start/RouteConfig.cs
@@ -10,7 +10,9 @@ namespace testAsp
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.EnableFriendlyUrls();
+            var settings = new FriendlyUrlSettings();
+            settings.AutoRedirectMode = RedirectMode.Permanent;
+            routes.EnableFriendlyUrls(settings);
         }
     }
 }
